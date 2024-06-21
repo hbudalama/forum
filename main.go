262fc40583f93e9a30e827d963fa6c2d2bb8c50a
zabdulla <zainabmadann@gmail.com>
2024-06-22 01:01:25 +0300

    server

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("/login", loginHandler)
+	// mux.HandleFunc("/api/posts/{id}/comments", )
+	// mux.HandleFunc("/api/posts", )
+	// mux.HandleFunc("/api/posts/{id}", )
+	// mux.HandleFunc("/api/posts/{id}/dislike", )
+	// mux.HandleFunc("/api/posts/{id}/like", )
+	// mux.HandleFunc("/api/categories", )
+	// mux.HandleFunc("signup", )
+	mux.HandleFunc("/", homeHandler)
+	// http://localhost:8080/login
+	log.Println("Serving on http://localhost:8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Println("Error starting server:", err)
+	}
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, filepath.Join("pages", "login.html"))
+		return
+	}
+
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, filepath.Join("pages", "index.html"))
+		return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
