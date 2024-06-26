91e3dd62d757775da9ecb9e3a7828c2947eae25c
zabdulla <zainabmadann@gmail.com>
2024-06-26 21:54:43 +0300

    more stuff

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	if !MethodsGuard(w, r, "POST") {
 		return
 	}
-	if LoginGuard( r) {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w,"Already logged in!")
+	if LoginGuard(r) {
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+
+	userID := r.FormValue("user_id")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(userID))
+	w.Write([]byte(email))
+	w.Write([]byte(hashedPassword))
 
 }
 
@@ -23,7 +38,10 @@ func CommentsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func AddPostsHandler(w http.ResponseWriter, r *http.Request) {
-
+	if !LoginGuard(r) {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
 }
 
 func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,11 +49,17 @@ func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func AddDislikesHandler(w http.ResponseWriter, r *http.Request) {
-
+	if !LoginGuard(r) {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
 }
 
 func AddLikesHandler(w http.ResponseWriter, r *http.Request) {
-
+	if !LoginGuard(r) {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
 }
 
 func GetCategoriesHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,8 +67,8 @@ func GetCategoriesHandler(w http.ResponseWriter, r *http.Request) {
 }
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if LoginGuard(r) {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w,"Already logged in!")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		fmt.Fprint(w, "Already logged in!")
 		return
 	}
 	if !MethodsGuard(w, r, "GET", "POST") {
