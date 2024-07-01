052d37a07c6a76c2403010e688a30121be3afb8e
Mohammed Adwan <mohammedadwan@icloud.com>
2024-07-01 22:12:54 +0300

    fetch posts functions

---
+++ pkg/db/fetch.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"learn.reboot01.com/git/hbudalam/forum/pkg/structs"
+)
+
+func getAllPosts() []structs.Post {
+	var posts []structs.Post
+
+	err := db.QueryRow("SELECT * FROM posts").Scan(&posts)
+
+	if err != nil {
+		return []structs.Post{}
+	}
+
+	return posts
+}
+
+func getFilteredPosts(category string) []structs.Post {
+	var filteredPosts []structs.Post
+	err := db.QueryRow("SELECT * FROM posts WHERE category = $1", category).Scan(&filteredPosts)
+
+	if err != nil {
+		return []structs.Post{}
+	}
+	return filteredPosts
+}
+
+func getPostDetails(postId int) (structs.Post, structs.User, []structs.Comment, []structs.Interaction) {
+	var (
+		thisPost          structs.Post
+		thisUser          structs.User
+		theseComments     []structs.Comment
+		theseInteractions []structs.Interaction
+	)
+
+	err := db.QueryRow("SELECT * FROM posts WHERE id = $1", postId).Scan(&thisPost.ID, &thisPost.Title, &thisPost.Content, &thisPost.Categories)
+
+	if err != nil {
+		return structs.Post{}, structs.User{}, []structs.Comment{}, []structs.Interaction{}
+	}
+
+	return thisPost, thisUser, theseComments, theseInteractions
+}
